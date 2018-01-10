54e8b14a67f7b28361a237262f293f662eef7c38
liubingguang <liubingguang>
2018-01-10 17:51:38 +0800

    log config

--- src/main/java/com/coderbike/controller/TestController.java
+++ src/main/java/com/coderbike/controller/TestController.java
@@ -2,6 +2,8 @@ package com.coderbike.controller;
 
 import com.coderbike.model.entity.User;
 import com.coderbike.service.AopThrowExceptionService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +19,8 @@ import javax.servlet.http.HttpServletRequest;
 @Controller
 @RequestMapping("/test")
 public class TestController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(TestController.class);
 
     @Value("${myname}")
     private String myName;
@@ -34,6 +38,15 @@ public class TestController {
     @ResponseBody
     public User testAop()  {
         return exceptionService.getUserById(null);
+    }
+
+    @RequestMapping("/log")
+    @ResponseBody
+    public String testLoggerLevel() {
+        LOGGER.info("this is info msg!");
+        LOGGER.debug("this is debug msg!");
+        LOGGER.warn("this is warn msg!");
+        return "ok";
     }
 
 }
--- src/main/java/com/coderbike/controller/UserController.java
+++ src/main/java/com/coderbike/controller/UserController.java
@@ -46,6 +46,4 @@ public class UserController {
         System.out.println(response);
         return new Response<>();
     }
-
-
 }
